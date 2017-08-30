8cde0398c56948e3a8d035df42514842ea1fe99f
Eva Jutzeler <eju@hoststar.ch>
2017-08-30 16:31:02 +0200

    Changing the navigation.js to a version Rhys has suggested.

--- js/navigation.js
+++ js/navigation.js
@@ -2,90 +2,92 @@
 // Text Love: JS > Navigation
 // -------------------------------------
 
-$( document ).ready(function() {
-	
+
+// ------------------------------
+// Changes to the header when it's not at the top of the page.
+// ------------------------------
+
+$(window).on('scroll', function () {
+	var scrollTop = $(window).scrollTop();
+
+	if (scrollTop > 100) {
+		$('.template__header').addClass('template__header--small');
+	} else {
+		$('.template__header').removeClass('template__header--small');
+	}
+});
+
+
+// ------------------------------
+// Change the top padding of the content depending on how high
+// the navigation header and footer is.
+// ------------------------------
+
+
+var headerHeight;
+var footerHeight;
+
+function resizeHeaderFooter() {
+	if ($(window).width() < 720) {
+		headerHeight = $('.template__header').outerHeight() + 20;
+	} else {
+		headerHeight = $('.template__header').outerHeight() + 30;
+	}
+	footerHeight = $('.template__footer').outerHeight() - 1;
+	return headerHeight, footerHeight;
+}
+
+
+$(window).resize(function () {
+	resizeHeaderFooter();
+	$('.template__content').css('padding-top', headerHeight + 'px');
+	$('.template__content').css('margin-bottom', footerHeight + 'px');
+});
+
+
+// ------------------------------
+// Feature Overlay Corrections
+// The overlay of the feature should be below the header.
+// ------------------------------
+
+
+$(window).resize(function () {
+	var headerHeight = $('.template__header').outerHeight();
+	var featureOverlayTopDistance = $('.widget[data-widget-type="feature"] .widget-overlay').attr('style', 'top: ' + headerHeight + 'px');
+});
+
+
+
+$(document).ready(function () {
 	// ------------------------------
 	// Change navigation if it matches tablet size.
 	// ------------------------------
-	
+
+
 	// Toggles class open to display/hide the whole navigation.
 
-	$('.navigation-toggle').click(function() {
+	$('.navigation-toggle').click(function () {
 		$('.widget[data-widget-type="extendednavigation"]').toggleClass('open');
 	});
 
 	// Toggles class open to display/hide folders.
 
-	$('.navigation-item.folder').each(function() {
-		$(this).click(function() {
+	$('.navigation-item.folder').each(function () {
+		$(this).click(function () {
 			$(this).parent('.navigation-item.folder').toggleClass('open');
 		});
 	});
 
-	$('.extendednavigation__navigation-toggle').click(function() {
+	$('.extendednavigation__navigation-toggle').click(function () {
 		$('html').toggleClass('nav-open');
 	});
-	
+
+
 	// ------------------------------
-	// Changes to the header when it's not at the top of the page.
+	// Hides other submenus when a main menu point with a submenu is clicked.
 	// ------------------------------
 
-	$(window).on('scroll', function () {
-		var scrollTop = $(window).scrollTop();
-
-		if (scrollTop > 100) {
-			$('.template__header').addClass('template__header--small');
-		} else {
-			$('.template__header').removeClass('template__header--small');
-		}
+	$(".navigation-item").click(function () {
+		$(this).siblings(".navigation-item").removeClass("folder-open open");
 	});
-
-	// ------------------------------
-	// Change the top padding of the content depending on how high
-	// the navigation header and footer is.
-	// ------------------------------
-
-	var headerHeight;
-	var footerHeight;
-
-	function resizeHeaderFooter() {
-		if ($(window).width() < 720) {
-			headerHeight = $('.template__header').outerHeight()+20;
-		} else {
-			headerHeight = $('.template__header').outerHeight()+30;
-		}
-		footerHeight = $('.template__footer').outerHeight()-1;
-		return headerHeight, footerHeight;
-	}
-
-	resizeHeaderFooter();
-	$('.template__content').css('padding-top', headerHeight +'px');
-	$('.template__content').css('margin-bottom', footerHeight +'px');
-
-	$(window).resize(function() {
-		resizeHeaderFooter();
-		$('.template__content').css('padding-top', headerHeight +'px');
-		$('.template__content').css('margin-bottom', footerHeight +'px');
-	});
-	
-	// ------------------------------
-	// Feature Overlay Corrections
-	// The overlay of the feature should be below the header.
-	// ------------------------------
-	
-	var headerHeight = $('.template__header').outerHeight();
-	var featureOverlayTopDistance = $('.widget[data-widget-type="feature"] .widget-overlay').attr('style', 'top: ' + headerHeight + 'px');
-
-	$(window).resize(function() {
-		var headerHeight = $('.template__header').outerHeight();
-		var featureOverlayTopDistance = $('.widget[data-widget-type="feature"] .widget-overlay').attr('style', 'top: ' + headerHeight + 'px');
-	});
-    
-	// ------------------------------
-    // Hides other submenus when a main menu point with a submenu is clicked.
-	// ------------------------------
-    
-    $(".navigation-item").click(function(){
-        $(this).siblings(".navigation-item").removeClass("folder-open open");
-    });
 });
